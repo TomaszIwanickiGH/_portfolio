f3432fc2c94b1f614658d388e28a8d02a030b9ff
Tomasz <tomasz.iwanicki@onet.pl>
2024-01-11 08:08:56 +0100

    Add some nav links

---
+++ src/constants/data.js
@@ -0,0 +1,24 @@
+const navLinks = [
+  {
+    number: '01.',
+    name: 'About',
+  },
+  {
+    number: '02.',
+    name: 'Stack',
+  },
+  {
+    number: '03.',
+    name: 'Experience',
+  },
+  {
+    number: '04.',
+    name: 'Projects',
+  },
+  {
+    number: '05.',
+    name: 'Contact',
+  },
+];
+
+export default navLinks;
