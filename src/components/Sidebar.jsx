bf068f69cd6583394bd8b511f5bcec97f47f256f
Tomasz <tomasz.iwanicki@onet.pl>
2024-01-11 08:10:02 +0100

    Start working on sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { images } from '../constants';
+import { TbBrandGithub } from 'react-icons/tb';
+import { BiLogoLinkedin } from 'react-icons/bi';
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="md:flex hidden flex-col justify-between items-center w-[8%] h-screen px-6 pt-8">
+      <div className="flex justify-center items-center w-[70px]">
+        <img
+          src={images.logo}
+          alt="logo"
+          className="w-full bg-cover"
+        />
+      </div>
+      <div className="flex flex-col gap-8 justify-center items-center">
+        <div className="flex flex-col gap-4">
+          <TbBrandGithub fontSize="30px" />
+          <BiLogoLinkedin fontSize="30px" />
+        </div>
+        <div className="w-[2px] flex justify-center items-center bg-neutral-400 h-[100px]" />
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
