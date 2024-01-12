6def8b307ea193bfda2b1be03ddedc6f084622b1
Tomasz <tomasz.iwanicki@onet.pl>
2024-01-12 11:39:38 +0100

    Fix paddings

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,20 +4,28 @@ import { BiLogoLinkedin } from 'react-icons/bi';
 
 const Sidebar = () => {
   return (
-    <div className="md:flex hidden flex-col justify-between items-center w-[8%] h-screen px-6 pt-8">
+    <div className="md:flex hidden flex-col justify-between items-center w-[8%] h-screen px-12 pt-8">
       <div className="flex justify-center items-center w-[70px]">
-        <img
-          src={images.logo}
-          alt="logo"
-          className="w-full bg-cover"
-        />
+        <a href="/">
+          <img
+            src={images.logo}
+            alt="logo"
+            className="w-full bg-cover"
+          />
+        </a>
       </div>
       <div className="flex flex-col gap-8 justify-center items-center">
         <div className="flex flex-col gap-4">
-          <TbBrandGithub fontSize="30px" />
-          <BiLogoLinkedin fontSize="30px" />
+          <TbBrandGithub
+            fontSize="30px"
+            color="gray"
+          />
+          <BiLogoLinkedin
+            fontSize="30px"
+            color="gray"
+          />
         </div>
-        <div className="w-[2px] flex justify-center items-center bg-neutral-400 h-[100px]" />
+        <div className="w-[2px] flex justify-center items-center bg-neutral-500 h-[100px]" />
       </div>
     </div>
   );
