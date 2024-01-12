65a565fca444056631762a053f63a5d23108e8fe
Tomasz <tomasz.iwanicki@onet.pl>
2024-01-12 11:38:29 +0100

    Give each link unique id

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,21 +1,26 @@
 const navLinks = [
   {
+    id: '#about',
     number: '01.',
     name: 'About',
   },
   {
+    id: '#stack',
     number: '02.',
     name: 'Stack',
   },
   {
+    id: '#experience',
     number: '03.',
     name: 'Experience',
   },
   {
+    id: '#projects',
     number: '04.',
     name: 'Projects',
   },
   {
+    id: '#contact',
     number: '05.',
     name: 'Contact',
   },
