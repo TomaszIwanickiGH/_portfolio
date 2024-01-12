f367d92303c3e6be50fc4529554ae40e246fd959
Tomasz <tomasz.iwanicki@onet.pl>
2024-01-12 11:38:49 +0100

    Create custom heading component

---
+++ src/components/Heading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Heading = ({ title, subtitle, id }) => {
+  return (
+    <section id={id}>
+      <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider">{subtitle}</p>
+      <h2 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] tracking-wider">{title}</h2>
+    </section>
+  );
+};
+
+export default Heading;
