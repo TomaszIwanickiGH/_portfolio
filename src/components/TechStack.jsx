6ea669144e2580e87ad25a15e0028f830b3e2498
Tomasz <tomasz.iwanicki@onet.pl>
2024-01-12 12:44:31 +0100

    Complete TechStack section

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,13 +1,32 @@
 import Heading from './Heading';
+import { data } from '../constants';
 
 const TechStack = () => {
   return (
-    <div className="flex flex-col gap-8 mt-20">
+    <div
+      className="flex flex-col gap-16 mt-20"
+      id="stack"
+    >
       <Heading
         id="#stack"
         subtitle="Tech Stack"
         title="My Skills."
       />
+      <div className="grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-16">
+        {data.skills.map((skill) => (
+          <div
+            className="flex items-center justify-center flex-col gap-4"
+            key={skill.name}
+          >
+            <img
+              src={skill.icon}
+              alt="icon"
+              className="w-[70px] h-[70px]"
+            />
+            <p className="">{skill.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
