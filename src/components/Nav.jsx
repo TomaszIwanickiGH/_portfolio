6c14b68e86b3c8d67361b330d7d0f571b8ea7efe
Tomasz <tomasz.iwanicki@onet.pl>
2024-01-12 11:39:18 +0100

    Create nav for mobile devices

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,32 +7,49 @@ const Nav = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
-    <div className="flex md:justify-end justify-between items-center md:p-12 p-8">
+    <div className="flex md:justify-end justify-between items-center">
       <div className="md:hidden flex justify-center items-center w-[70px]">
-        <img
-          src={images.logo}
-          alt="logo"
-          className="w-full bg-cover"
-        />
+        <a href="/">
+          <img
+            src={images.logo}
+            alt="logo"
+            className="w-full bg-cover"
+          />
+        </a>
       </div>
-      <div className="md:flex hidden justify-end items-center gap-6">
+      <div className="sm:flex hidden justify-end items-center gap-6">
         {navLinks.map((link) => (
-          <div
+          <a
             key={link.name}
-            className="flex gap-2"
+            href={link.id}
+            className="flex gap-2 hover:cursor-pointer"
           >
             <span className="text-secondary">{link.number}</span>
             <p>{link.name}</p>
-          </div>
+          </a>
         ))}
       </div>
       <GiHamburgerMenu
-        className="md:hidden flex"
+        onClick={() => setToggleMenu((prev) => !prev)}
+        className="sm:hidden flex hover:cursor-pointer"
         fontSize="28px"
         color="white"
       />
 
-      {toggleMenu && <div></div>}
+      {toggleMenu && (
+        <div className="flex flex-col gap-6 absolute top-[100px] right-[50px] p-4 border-[2px] border-secondary rounded-2xl z-50">
+          {navLinks.map((link) => (
+            <a
+              key={link.name}
+              href={link.id}
+              className="flex gap-2 min-w-[200px] hover:cursor-pointer border-b-[1px] border-secondary border-dashed pb-4"
+            >
+              <span className="text-secondary">{link.number}</span>
+              <p>{link.name}</p>
+            </a>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
