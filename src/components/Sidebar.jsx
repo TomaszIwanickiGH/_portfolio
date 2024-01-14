e5d12a2a6a69a039969d395b5a224f9075f13df0
Tomasz <tomasz.iwanicki@onet.pl>
2024-01-14 19:39:40 +0100

    Chane color on hover

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -19,14 +19,20 @@ const Sidebar = () => {
           <p className="-rotate-90 tracking-wide text-neutral-400">tomasz.iwanicki@onet.pl</p>
         </div>
         <div className="flex flex-col gap-4">
-          <TbBrandGithub
-            fontSize="30px"
-            color="gray"
-          />
-          <BiLogoLinkedin
-            fontSize="30px"
-            color="gray"
-          />
+          <a
+            href="https://github.com/TomaszIwanickiGH"
+            target="_blank"
+            className=" hover:text-white text-neutral-400"
+          >
+            <TbBrandGithub fontSize="30px" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/tomasz-iwanicki-765596278/"
+            target="_blank"
+            className=" hover:text-white text-neutral-400"
+          >
+            <BiLogoLinkedin fontSize="30px" />
+          </a>
         </div>
         <div className="w-[2px] flex justify-center items-center bg-neutral-500 h-[100px]" />
       </div>
